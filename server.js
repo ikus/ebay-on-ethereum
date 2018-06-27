26b53e10ff7cf2e25411f2451e5e9efdf47d2fa0
Emisael Calderon <emisael.calderon@indiciumsolutions.com.mx>
2018-06-27 00:25:02 -0500

    Add Events listener.

--- server.js
+++ server.js
@@ -1,3 +1,10 @@
+var ecommerce_store_artifacts = require('./build/contracts/EcommerceStore.json');
+var contract = require('truffle-contract');
+var web3 = require('web3');
+var provider = new web3.providers.HttpProvider("http://localhost:8545");
+var EcommerceStore = contract(ecommerce_store_artifacts);
+EcommerceStore.setProvider(provider);
+
 var express = require('express');
 
 var app = express();
@@ -10,3 +17,19 @@ app.listen(3000, function(){
 app.get('/',function(req,res){
 	res.send("Hello, Ethereum!");
 });
+
+setupProductEventlistener();
+
+function setupProductEventlistener(){
+	var productEvent;
+	EcommerceStore.deployed().then(function(i){
+		productEvent = i.NewProduct({fromBlock: 0, toBlock:'latest'})
+		productEvent.watch(function(err,result){
+			if(err){
+				console.log(err);
+			}else{
+				console.log(result.args);
+			}
+		});
+	});
+}
