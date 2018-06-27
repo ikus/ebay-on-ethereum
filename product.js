07c8cf64dd0dcc940cedb803abf457677d0cab5f
Emisael Calderon <emisael.calderon@indiciumsolutions.com.mx>
2018-06-27 12:04:13 -0500

    Test in mongodb

--- product.js
+++ product.js
@@ -1,6 +1,25 @@
 var mongoose = require('mongoose');
 
+
+
+
+//Set up default mongoose connection
+var mongoDB = 'mongodb://127.0.0.1/my_database';
+mongoose.connect(mongoDB);
+
+
+
+
 mongoose.Promise = global.Promise;
+
+//Get the default connection
+var db = mongoose.connection;
+
+//Bind connection to error event (to get notification of connection errors)
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+
+
 
 var Schema = mongoose.Schema;
 
@@ -17,7 +36,8 @@ var ProductSchema = new Schema({
 	buyer: String
 });
 
-var ProductModel = mongoose.model('ProductModel',ProductSchema);
+var ProductModel = mongoose.model('ProductModel',ProductSchema,'ProductModels');
 
 module.exports = ProductModel;
+
 
--- server.js
+++ server.js
@@ -5,6 +5,10 @@ var provider = new web3.providers.HttpProvider("http://localhost:8545");
 var EcommerceStore = contract(ecommerce_store_artifacts);
 EcommerceStore.setProvider(provider);
 
+var mongoose = require('mongoose');
+var ProductModel = require('./product');
+//var SomeModel = require('../models/somemodel')
+
 var express = require('express');
 
 var app = express();
@@ -28,8 +32,39 @@ function setupProductEventlistener(){
 			if(err){
 				console.log(err);
 			}else{
-				console.log(result.args);
+				//console.log(result.args);
+				saveProduct(result.args);
 			}
 		});
 	});
 }
+
+function saveProduct(product){
+	console.log("Saving: "+product._productId);
+	//console.log("ProductModel: "+ProductModel);
+
+	//ProductModel.findOne({'blockChainId': product._productId.toNumber()}).then(function(dbProduct){console.log("<<<>>>"+dbProduct);});
+
+	ProductModel.findOne({'blockChainId': product._productId},function(err,dbProduct){
+		console.log("Count : "+product);
+		if(dbProduct!=null){
+			console.log(dbProduct);
+			return;
+		}
+
+		var p = new ProductModel({name: product._name, blockChainId: product._productId, 
+			category:product._category ,ipfsImageHash: product._imageLink, ipfsDeschash: product._desLink, 
+			startTime: product._startTime, price: product._price , condition: product._productCondition});
+
+		p.save(function(error){
+			if(error){
+				console.log(error);
+			}else{
+				ProductModel.count({},function(err,count){
+					console.log("Count is: "+count);
+				});
+			}
+		});
+	})
+}
+
