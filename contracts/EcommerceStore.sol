bba126758ce2d974da5ae9509392beecc9344c3a
Emisael Calderon <emisael.calderon@indiciumsolutions.com.mx>
2018-06-26 23:40:47 -0500

    add Events on Contract

--- contracts/EcommerceStore.sol
+++ contracts/EcommerceStore.sol
@@ -23,6 +23,10 @@ contract EcommerceStore{
         address buyer;    
     }
     
+
+    event NewProduct(uint _productId,string _name,string _category,string _imageLink,string _descLink,uint _startTime,uint _price,uint _productCondition);
+    
+
     constructor(address _arbiter) public {
         productIndex =0;        
         arbiter=_arbiter;
@@ -35,6 +39,7 @@ contract EcommerceStore{
             _descLink,_startTime,_price,ProductCondition(_productCondition),0);
         stores[msg.sender][productIndex] = product;    
         productIdInStore[productIndex] = msg.sender ;
+        emit NewProduct(productIndex,_name,_category,_imageLink,_descLink,_startTime,_price,_productCondition);
     }    
     
     function getProduct(uint _productId) public view returns (uint,string,string,string,
